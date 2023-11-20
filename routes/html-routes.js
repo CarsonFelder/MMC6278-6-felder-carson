2bc10233052d36df67806a33c73894baf47d8619
CarsonFelder <carson.felder99@gmail.com>
2023-11-19 21:52:16 -0500

    found missed TODO!! 146 P and 7 F!!

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -42,7 +42,7 @@ router.get('/cart', async (req, res) => {
     .toFixed(2)
 
   // TODO: Convert the response below to render a handlebars template
-  res.sendFile(path.join(__dirname, '../views/cart.html'))
+  res.render('cart',{cartItems, total})
 })
 
 module.exports = router
